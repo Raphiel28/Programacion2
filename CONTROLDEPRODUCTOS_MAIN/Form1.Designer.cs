040a0fbee5332297e32fc1855dd6c3437634a847
Isaac-Claudio <aisackpluss@gmail.com>
2020-10-09 21:46:54 -0400

    Main view

--- CONTROLDEPRODUCTOS_MAIN/Form1.Designer.cs
+++ CONTROLDEPRODUCTOS_MAIN/Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
@@ -47,34 +48,35 @@
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(804, 458);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(796, 432);
+            this.tabPage1.Size = new System.Drawing.Size(796, 429);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Mantenimiento de Productos";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(796, 432);
+            this.tabPage2.Size = new System.Drawing.Size(796, 429);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Mantenimiento de Categorias";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(796, 432);
+            this.tabPage3.Size = new System.Drawing.Size(796, 429);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Todos los Productos";
             this.tabPage3.UseVisualStyleBackColor = true;
